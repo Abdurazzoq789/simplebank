f3e40421a6dfe87dd9668c92b2b485a70b621ad6
Abdurazzoq <arasulmuhammedov@gmail.com>
2021-06-03 19:15:30 +0500

    user create api /

--- api/server.go
+++ api/server.go
@@ -20,6 +20,8 @@ func NewServer(store db.Store)	*Server {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	router.POST("/users", server.createUser)
+
 	//accounts
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
