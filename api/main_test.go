7215212c0d215dfa131672ad3b3ef8797f80ee76
Abdurazzoq <arasulmuhammedov@gmail.com>
2021-06-05 12:03:19 +0500

    create token api done

--- api/main_test.go
+++ api/main_test.go
@@ -1,10 +1,25 @@
 package api
 
 import (
+	db "github.com/Abdurazzoq789/simplebank/db/sqlc"
+	"github.com/Abdurazzoq789/simplebank/util"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/require"
 	"os"
 	"testing"
+	"time"
 )
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey: util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
+}
 
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
--- api/server.go
+++ api/server.go
@@ -1,26 +1,46 @@
 package api
 
 import (
+	"fmt"
 	db "github.com/Abdurazzoq789/simplebank/db/sqlc"
+	"github.com/Abdurazzoq789/simplebank/token"
+	"github.com/Abdurazzoq789/simplebank/util"
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 )
 
 type Server struct {
+	config util.Config
 	store  db.Store
+	tokenMaker token.Maker
 	router *gin.Engine
 }
 
-func NewServer(store db.Store)	*Server {
-	server := &Server{store: store}
-	router := gin.Default()
+func NewServer(config util.Config, store db.Store)	(*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		config: config,
+		store: store,
+		tokenMaker: tokenMaker,
+	}
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
 		v.RegisterValidation("currency", validCurrency)
 	}
 
+	server.setupRouter()
+	return server, nil
+}
+
+func (server *Server) setupRouter()  {
+	router := gin.Default()
+
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
 
 	//accounts
 	router.POST("/accounts", server.createAccount)
@@ -31,7 +51,6 @@ func NewServer(store db.Store)	*Server {
 	router.POST("/transfers", server.createTransfer)
 
 	server.router = router
-	return server
 }
 
 func (server *Server) Start(address string) error {
